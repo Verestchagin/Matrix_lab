e5bf8ab82579cd545dbc2b0b24708e9754d24644
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-04-21 15:47:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-
-#include <matrix.hpp>
+#include "matrix.hpp"
 
 int main() {
 
-    Matrix m1{3,5};
-    Matrix m2{1, 2, 3, 4, 5, 6};
-    Matrix m3{m2};
+    Matrix_t m1{3,5};
+    Matrix_t m2{1, 2, 3, 4, 5, 6};
 
-    cout << m1 << endl; 
+    Matrix_t m3{m2};
+
+    cout << m1 << endl;
     cout << m2 << endl;
     cout << m3 << endl;
 
-    Matrix m4;
+    Matrix_t m4;
     m4 = m3;
     cout << m4 << endl;
 
@@ -20,20 +20,24 @@ int main() {
 
     cout << boolalpha  << (m2 == m3) << (m3 == m4) << endl;
 
-    cout << m1.rows() << m1.columns() << endl; 
+    cout << m1.rows() << m1.columns() << endl;
 
-    Matrix m5{0, 2, 4, 6, 8, 10};    
-    cout << m3 + m5 << endl; 
+    Matrix_t m5{0, 2, 4, 6, 8, 10};
+    cout << m3 + m5 << endl;
     cout << m3 - m5 << endl;
 
-    Matrix m6{}; 
-    cout << m6.empty();
+    Matrix_t m6{};
+    cout << m6.empty()<<endl;
 
     ofstream out{"matrix.bin"};
     out << m3;
 
     ifstream in{"matrix.bin"};
     in >> m6;
+
     m3.swap_(m6);
+
     cout << m3 << endl;
+
+
 }
