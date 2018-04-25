969d5dd33ede085acaad9a4cd22b30a4d4ea86d2
Verestchagin <32384378+Verestchagin@users.noreply.github.com>
2018-04-25 16:32:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,15 @@
 
 int main() {
 
-    Matrix_t m1{3,5};
-    Matrix_t m2{1, 2, 3, 4, 5, 6};
-
-    Matrix_t m3{m2};
+    Matrix <int, 2, 1> m1{3,5};
+    Matrix <int, 3, 2> m2{1, 2, 3, 4, 5, 6};
+    Matrix <int, 3, 2> m3{m2};
 
     cout << m1 << endl;
     cout << m2 << endl;
     cout << m3 << endl;
 
-    Matrix_t m4;
+    Matrix <int, 3, 2> m4;
     m4 = m3;
     cout << m4 << endl;
 
@@ -20,13 +19,16 @@ int main() {
 
     cout << boolalpha  << (m2 == m3) << (m3 == m4) << endl;
 
-    cout << m1.rows() << m1.columns() << endl;
+    cout << m1.rows() << "" << m1.columns() << endl;
+    cout << endl;
 
-    Matrix_t m5{0, 2, 4, 6, 8, 10};
+    Matrix <int, 3, 2> m5{0, 2, 4, 6, 8, 10};
+    cout << m5 << endl;
+    cout << endl;
     cout << m3 + m5 << endl;
     cout << m3 - m5 << endl;
 
-    Matrix_t m6{};
+    Matrix <int, 3, 2> m6{};
     cout << m6.empty()<<endl;
 
     ofstream out{"matrix.bin"};
@@ -35,6 +37,7 @@ int main() {
     ifstream in{"matrix.bin"};
     in >> m6;
 
+    m6[0][0] = 145;
     m3.swap_(m6);
 
     cout << m3 << endl;
